f5d57c005828be83430626e4820a26bcb7c67261
ZSaberLv0 <z@zsaber.com>
2021-12-05 16:07:50 +0800

    update

--- ZFModuleDemo_app/zfproj_with_src/Android/ZFModuleDemo_app/zfapp/src/main/java/com/ZFFramework/Android/ZFModuleDemo_app/Loader.java
+++ ZFModuleDemo_app/zfproj_with_src/Android/ZFModuleDemo_app/zfapp/src/main/java/com/ZFFramework/Android/ZFModuleDemo_app/Loader.java
@@ -1,31 +1,24 @@
 package com.ZFFramework.Android.ZFModuleDemo_app;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+
 import com.ZFFramework.Android.ZF_impl.ZFMainEntry;
 
 public class Loader extends Activity {
     static {
         System.loadLibrary("ZFModuleDemo_app");
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         if(BuildConfig.DEBUG) {
             ZFMainEntry.debugMode(true);
         }
-    }
-    private boolean _firstTime = true;
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if(_firstTime) {
-            _firstTime = false;
-            Intent intent = new Intent(Loader.this, ZFMainEntry.class);
-            startActivity(intent);
-            Loader.this.finish();
-        }
+        ZFMainEntry.mainEntryFromLoader(this);
     }
 }
 
