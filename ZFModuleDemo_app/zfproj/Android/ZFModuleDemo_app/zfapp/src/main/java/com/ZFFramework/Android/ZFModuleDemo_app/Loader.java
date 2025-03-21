078f7d75731a823a60b6b6543f2f21d0b183e9b2
ZSaberLv0 <z@zsaber.com>
2025-03-22 00:56:55 +0800

    update

--- ZFModuleDemo_app/zfproj/Android/ZFModuleDemo_app/zfapp/src/main/java/com/ZFFramework/Android/ZFModuleDemo_app/Loader.java
+++ ZFModuleDemo_app/zfproj/Android/ZFModuleDemo_app/zfapp/src/main/java/com/ZFFramework/Android/ZFModuleDemo_app/Loader.java
@@ -1,24 +1,12 @@
 package com.ZFFramework.Android.ZFModuleDemo_app;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Window;
+import com.ZFFramework.Android.ZF_impl.ZFLoader;
 
-import com.ZFFramework.Android.ZF_impl.ZFMainEntry;
+public class Loader extends ZFLoader {
 
-public class Loader extends Activity {
     static {
         System.loadLibrary("ZFModuleDemo_app");
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        super.onCreate(savedInstanceState);
-        if (BuildConfig.DEBUG) {
-            ZFMainEntry.debugMode(true);
-        }
-        ZFMainEntry.mainEntryFromLoader(this);
-    }
 }
 
