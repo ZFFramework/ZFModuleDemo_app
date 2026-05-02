2b5bfa8962660cbe7b1257308421d0e32d2e656c
ZSaberLv0 <z@zsaber.com>
2026-05-02 15:58:47 +0800

    update

--- ZFModuleDemo_app/zfproj_with_src/Android/ZFModuleDemo_app/zfapp/src/main/java/com/ZFFramework/ZFModuleDemo_app/ZFLoader.java
+++ ZFModuleDemo_app/zfproj_with_src/Android/ZFModuleDemo_app/zfapp/src/main/java/com/ZFFramework/ZFModuleDemo_app/ZFLoader.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.Window;
 
 import com.ZFFramework.ZF_impl.ZFMainEntry;
-import com.ZFFramework.ZFModuleDemo_app.BuildConfig;
 
 public class ZFLoader extends Activity {
     @Override
